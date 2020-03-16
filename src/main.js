8649ff000b6e220d41f5046898c7e9b74793830a
dujinyang <645869563@qq.com>
2020-03-16 15:59:23 +0800

    热身结束

--- src/main.js
+++ src/main.js
@@ -1,15 +1,20 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import fastClick from "fastclick";
+import "./assets/styles/reset.css";
+import "./assets/styles/border.css";
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
-
+fastClick.attach(document.body);
+// 1像素边框问题
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   components: { App },
-  template: '<App/>'
-})
+  template: "<App/>"
+});
+//路由就是根据网址的不同，来展示不同的网页
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,13 +1,11 @@
 <template>
-  <div id="app">
-    <router-view />
+  <div>
+    hello world
   </div>
 </template>
-
 <script>
 export default {
-  name: "App"
+  name: "Home"
 };
 </script>
-<style>
-</style>
+<style></style>
