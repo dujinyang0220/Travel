a339405195f7c21997fc04e7fc2c48462accda79
dujinyang <645869563@qq.com>
2020-03-18 19:11:50 +0800

    修正
    有些效果实现不好，进行了校正

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -5,15 +5,16 @@
       v-for="item of letters"
       :key="item"
       :ref="item"
-      @click="handleLetterClick"
-      @touchstart="handleTouchStart"
+      @touchstart.prevent="handleTouchStart"
       @touchmove="handleTouchMove"
       @touchend="handleTouchEnd"
+      @click="handleLetterClick"
     >
       {{ item }}
     </li>
   </ul>
 </template>
+
 <script>
 export default {
   name: "CityAlphabet",
@@ -32,11 +33,12 @@ export default {
   data() {
     return {
       touchStatus: false,
-      startY: 0
+      startY: 0,
+      timer: null
     };
   },
   updated() {
-    this.startY = his.$refs["A"][0].offsetTop;
+    this.startY = this.$refs["A"][0].offsetTop;
   },
   methods: {
     handleLetterClick(e) {
@@ -50,34 +52,35 @@ export default {
         if (this.timer) {
           clearTimeout(this.timer);
         }
-        this.timer=setTimeout(()=>{
-        const touchY = e.touches[0].clientY - 160;
-        const index = Math.floor((touchY - this.startY) / 20);
-        if ((index >= 0) & (index < this.letters.length)) {
-          this.$emit("change", this.letters[index]);
-        }
-      },16})
+        this.timer = setTimeout(() => {
+          const touchY = e.touches[0].clientY - 79;
+          const index = Math.floor((touchY - this.startY) / 20);
+          if (index >= 0 && index < this.letters.length) {
+            this.$emit("change", this.letters[index]);
+          }
+        }, 16);
+      }
     },
     handleTouchEnd() {
-      this.touchStatus = true;
+      this.touchStatus = false;
     }
   }
 };
 </script>
-<style lang="stylus" scoped>
-@import '~styles/varibles.styl';
-.list
- display: flex
- flex-direction: column
- justify-content: center
- position: absolute
- top:1.58rem
- right:0
- bottom: 0
- width: .4rem
 
- .item
-  line-height: .4rem
-  text-align:center
-  color: $bgColor
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.list
+  display: flex
+  flex-direction: column
+  justify-content: center
+  position: absolute
+  top: 1.58rem
+  right: 0
+  bottom: 0
+  width: .4rem
+  .item
+    line-height: .4rem
+    text-align: center
+    color: $bgColor
 </style>
